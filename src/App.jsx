0428ebfcdbbb35a8b4b4cdf7625b86819c40feb7
Piotr Poterek <peterpoterek0@gmail.com>
2023-07-22 23:53:15 +0200

    removed unused imports from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import "./App.css";
-import axios from "axios";
 import PokeAPI from "./Components/PokeAPI";
 import { QueryClient, QueryClientProvider } from "react-query";
 
